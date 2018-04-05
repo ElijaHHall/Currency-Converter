1e46bf883b3dec2828565f2c719091a9dff0a0ee
ElijahHHall <ehall851@gmail.com>
2018-04-05 07:13:02 -0600

    Currency is now converting

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 
-
-$.ajax ({
-	method:"get",
-	url:("http://data.fixer.io/api/latest?access_key=7a6637717fc1790c29a2f7ea97a1044e");
-	datatype: "json",
-});
+$(document).ready(function() {
 
 
+const api = "http://data.fixer.io/api/latest?access_key=7a6637717fc1790c29a2f7ea97a1044e"
+	
+		$.get(api, function(response) {
+			console.log(response);
 
-function convertCurrency(event) {
-  if ('type' in event.target && event.target.type === 'button') {
-    console.log('handle currency conversion here');
-  }
-}
+			$("input[type=button]").on("click", function(event){
+					console.log(event);
 
-document.body.addEventListener('click', convertCurrency);
+					countryCurrency = $(this).val();
+
+					response.rates[countryCurrency];
+
+					let monies = $("input[type=text]").val();
+					let eurMonies = monies / response.rates["USD"];
+					let lastMonies = eurMonies * response.rates[countryCurrency];
+
+					$("#output_amount").append("<p>" + lastMonies + "" + countryCurrency + ".<p>");
+
+			});
+
+	});
+
+});	
+
+
+
+
+
+
+
+
+// function convertCurrency(event) {
+//   if ('type' in event.target && event.target.type === 'button') {
+//     console.log('handle currency conversion here');
+//   }
+
+
+
+// }
+
+
+// });
+// document.body.addEventListener('click', convertCurrency);
+
